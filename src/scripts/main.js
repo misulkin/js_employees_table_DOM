07ac24e76bcc6c98d29c58071971d23579cfe2db
Artur Misko <arthurmisko@gmail.com>
2025-09-17 20:18:32 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,7 @@
 
 // write code her
 
+// eslint-disable-next-line no-unused-vars
 const list = document.getElementsByTagName('table');
+// eslint-disable-next-line no-unused-vars
+const el = document.getElementsByTagName('a');
